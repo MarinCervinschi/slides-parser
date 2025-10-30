c53b6a3dcde334a6aa786c5854ecf293c9a0ff23
MarinCervinschi <marincervinschi3@gmail.com>
2025-10-30 17:10:58 +0100

    feat: add jest-fetch-mock for enhanced testing and include web-streams-polyfill dependency

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,11 @@
 import "@testing-library/jest-dom";
+import fetchMock from "jest-fetch-mock";
+
+fetchMock.enableMocks();
+
+if (typeof global.TransformStream === "undefined") {
+	const { TransformStream } = jest.requireActual(
+		"web-streams-polyfill/dist/ponyfill.js"
+	);
+	global.TransformStream = TransformStream;
+}
